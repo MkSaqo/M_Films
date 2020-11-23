b8b7e7dc47efe36dabe680187460853faedeee8b
abmkrtchyan <albert.mkrtchyan.98@mail.ru>
2020-11-23 17:06:08 +0400

    header2in animacia avelacrec

--- tpl/header2.php
+++ tpl/header2.php
@@ -1,22 +1,20 @@
-<?php 
+ <?php 
 include "functions.php";
 include "tpl/connect.php";
-$s = "SELECT * FROM `info` WHERE `info_category` = 'video'";
-$video = mysqli_query($db,$s);
-$vid = mysqli_fetch_all($video,MYSQLI_ASSOC);
-$sql = "SELECT * FROM `kinoner` ORDER BY `id` DESC ";
-$conn = mysqli_query($db,$sql);
-$i = 0;
-while(++$i<8){
-	$result = mysqli_fetch_assoc($conn) ;
-	$id = $result["id"];
-	$home_img = $result["home_img"];
-}
+// $sql = "SELECT * FROM `kinoner` ORDER BY `id` DESC ";
+// $conn = mysqli_query($db,$sql);
+// $i = 0;
+// while(++$i<8){
+// 	$result = mysqli_fetch_assoc($conn) ;
+// 	$id = $result["id"];
+// 	$home_img = $result["home_img"];
+// }
 ?>
 <!DOCTYPE html>
 <html class="html_filmer1">
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  		
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link rel="icon" href="nkarner/logo.jpg">
@@ -26,24 +24,19 @@ while(++$i<8){
 	<body>
 		<div id="vid-div">
 			<video id="_vidElement" class="video_all_screen" muted loop src="nkarner/best.mp4" type="video/mp4"></video>
-			<!-- <div class="aaaaa"></div> -->
-			<div class="header2-logo">
-				  <a href="#section02">
-				<img src="nkarner/logo.jpg" alt="">
-				<p>Welcome to our virtual cinema M_Films.</p>
-				<section id="section01" class="demo">
-				  &darr;
-				</section>
-				</a>
-			</div>
+			<a href="#section02">
+				<div class="header2-logo animate__animated animate__flash">
+					<img src="nkarner/logo.jpg" alt="">
+					<p>Welcome to our virtual cinema M_Films.</p>
+					<section class="demo">&darr;</section>
+				</div>
+			</a>
 		</div>
+
 		<script type="text/javascript">
 			var vid = document.getElementById('_vidElement');
-			// var vid_div = document.getElementById('vid-div');
-			// vid_div.style.height = screen.height-1000;
 			$(document).ready(function(){
-				if (vid.paused)
-        			vid.play();				
+				if (vid.paused)vid.play();				
 			});	
 			$(function() {
 			    $('a[href*=\\#]').on('click', function(e) {
