df0cf94cc24c96c13f074372b45efac7e4683c1b
abmkrtchyan <albert.mkrtchyan.98@mail.ru>
2020-12-01 01:05:03 +0400

    .htaccess sarqeci

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@ include "tpl/header.php";
 	<div class="container1">
 		<div class="index_left">
             <div class="img404">
-            
+            	
             </div>
 		</div>
 <?php include "tpl/right.php" ;?>
--- film.php
+++ film.php
@@ -120,16 +120,17 @@
 					var myIndex = 0;
 					carousel();
 					function carousel() {
-		  var i;
-		  var x = document.getElementsByClassName("mySlides");
-		  for (i = 0; i < x.length; i++) {
-		    x[i].style.display = "none";  
-		  }
-		  myIndex++;
-		  if (myIndex > x.length) {myIndex = 1}    
-		  x[myIndex-1].style.display = "block";  
-		  setTimeout(carousel, 2000); 
-		}
+					  var i;
+					  var x = document.getElementsByClassName("mySlides");
+					  for (i = 0; i < x.length; i++) {
+					    x[i].style.display = "none";  
+					  }
+					  console.log(1)
+					  myIndex++;
+					  if (myIndex > x.length) {myIndex = 1}    
+					  x[myIndex-1].style.display = "block";  
+					  setTimeout(carousel, 2000); 
+					}
 				</script>
 			<?php include "tpl/comments.php" ;
 				}else{
